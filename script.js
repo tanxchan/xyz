0d8f756ed51cfe0b57220f01aa137eae9006e12c
Tiancheng Shao <39867814+tanxchan@users.noreply.github.com>
2024-11-23 17:57:16 -0500

    made more professional

--- script.js
+++ script.js
@@ -1,25 +1,14 @@
 const coords = {x:0, y:0}
-const u = document.getElementsByClassName("underlay")[0];
-//var c = document.createElement("div");
-function fadeOutAndRemove(element){
-    //element.classList.toggle('fade-out')
-}
+let c = document.createElement("div")
+c.setAttribute("class", "cursor-circle")
+setTimeout(()=>{
+    let t = document.getElementById('top-of-page')
+    t.appendChild(c);},10)
 function a(e){//i love chat
     coords.x = e.clientX;
     coords.y = e.clientY;
-    let c = document.createElement("div")
-    c.setAttribute("class", "cursor-circle")
-    c.style.left= coords.x-12+'px'
-    c.style.top= coords.y-12+'px'
-    document.body.insertBefore(c, u)
-   // console.log('created', c);
-    setTimeout(() => {
-        c.classList.add('fade-out');
-    }, 10);
-    c.addEventListener('transitionend', function(){
-        //console.log("hello");
-        c.parentNode.removeChild(c);
-    })
-
+    c.style.left= coords.x+'px';
+    c.style.top= coords.y+'px';
+    console.log(c);
 }
 window.addEventListener("mousemove", a)//make a little circle to follow the cursor around :o
